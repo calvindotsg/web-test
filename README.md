9262e94ecca74d291d59288cef3d57f299af6487
Calvin <calvin@Calvins-MacBook-Pro.local>
2023-09-01 19:33:37 +0800

    Initial commit with main, unit tests and README.md

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# web-test
-Automated web testing and scraper for search result metrics
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <h1 align="center">Automated web testing and scraper for search result metrics</h1>
+</p>
+<!-- PROJECT LOGO -->
+
+[![GitHub stars](https://img.shields.io/github/stars/calvindotsg/auto-web-test)](./portfolio/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/calvindotsg/auto-web-test)](./portfolio/network)
+[![GitHub issues](https://img.shields.io/github/issues/calvindotsg/auto-web-test)](./portfolio/issues)
+[![GitHub license](https://img.shields.io/github/license/calvindotsg/auto-web-test)](./portfolio/blob/master/LICENSE)
+
+## Overview
+
+For the purpose of automated web testing with Selenium, this Python project automates the process of web scraping specific elements from a website based on a list of keywords. The program navigates to a specified search URL for each keyword, captures a screenshot, and extracts specific text elements using BeautifulSoup. The captured metrics are stored in an output CSV file for further analysis.
+
+## Technologies Used
+
+- Python
+- Selenium WebDriver
+- BeautifulSoup
+- Pandas
+
+## Requirements
+
+To run this project, you need to install the following Python packages:
+
+```bash
+pip install selenium pandas beautifulsoup4
+```
+
+Also, make sure to download the appropriate [WebDriver](https://www.selenium.dev/documentation/en/webdriver/driver_requirements/) for the browser you are using.
+
+## Features
+
+- Captures page load time for each keyword search.
+- Takes a screenshot for each keyword's search result.
+- Extracts and stores specific text elements like logo, MCC, and Merchant Name.
+- Logs all the metrics into an output CSV file.
+
+## How to Use
+
+1. Clone this repository.
+2. Add a CSV file named `top5.csv` in the `assets` directory, with a column named `keyword` listing the keywords you want to test.
+3. Update the `website` variable to point to your target search URL.
+4. Run the script:
+
+```bash
+python main.py
+```
+
+## Code Structure
+
+The code is organized into modular functions, each responsible for a specific task:
+
+- `initialize_driver()`: Initializes WebDriver.
+- `read_keywords_from_csv(file_path)`: Reads keywords from the input CSV file.
+- `capture_page_load_time(start_time)`: Captures page load time.
+- `capture_http_status(driver)`: Captures HTTP status code.
+- `take_screenshot(driver, keyword, file_name)`: Takes screenshots.
+- `save_metrics(df, index, page_load_time, elements_retrieved)`: Saves metrics into a DataFrame.
+- `find_elements_from_source(soup_string, str1, sub2)`: Extracts specific text elements.
+- `retrieve_elements(driver)`: Captures number of results and other text elements.
+- `export_to_csv(df, output_file_path)`: Exports the DataFrame to an output CSV file.
+- `main()`: Main function to control the flow.
+
+## License
+
+This project is open-source and available under the MIT License.
+
+## Contributing
+
+Contributions are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Author
+
+- [Calvin](https://calvin.sg)
---
+++ main.py
@@ -0,0 +1,136 @@
+# Importing required libraries
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import time
+import yaml
+
+with open("config.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+inputFile = config["inputFile"]
+outputFile = config["outputFile"]
+website = config["website"]
+average_page_load_time = config["average_page_load_time"]
+
+substring_to_find_elements = {
+    "logo": {
+        "str1": config["logo_str1"],
+        "str2": config["logo_str2"]
+    },
+    "mcc": {
+        "str1": config["mcc_str1"],
+        "str2": config["mcc_str2"]
+    },
+    "merchant_name": {
+        "str1": config["merchant_name_str1"],
+        "str2": config["merchant_name_str2"]
+    }
+}
+
+
+# Initialize WebDriver
+def initialize_driver():
+    driver = webdriver.Chrome()
+    return driver
+
+
+# Read keywords from CSV
+def read_keywords_from_csv(file_path):
+    return pd.read_csv(file_path)
+
+
+# Capture page load time
+def capture_page_load_time(start_time):
+    return time.time() - start_time
+
+
+# Capture HTTP status code (using readyState as a proxy)
+def capture_http_status(driver):
+    return driver.execute_script("return document.readyState;")
+
+
+# Take screenshot
+def take_screenshot(driver, keyword, file_name):
+    driver.save_screenshot('output/' + keyword + '_' + file_name)
+
+
+# Save metrics to DataFrame
+def save_metrics(df, index, page_load_time, elements_retrieved):
+    df.at[index, 'Page Load Time'] = page_load_time
+    df.at[index, 'Logo'] = elements_retrieved["logo"]
+    df.at[index, 'MCC'] = elements_retrieved["mcc"]
+    df.at[index, 'Merchant Name'] = elements_retrieved["merchant_name"]
+    # df.at[index, 'HTTP Status Code'] = http_status_code
+
+
+def find_elements_from_source(soup_string, str1, sub2):
+    # Logo: initializing substrings
+
+    # getting index of substrings
+    idx1 = soup_string.index(str1)
+    idx2 = soup_string.index(sub2)
+
+    res = ''
+    # getting elements in between
+    for idx in range(idx1 + len(str1), idx2):
+        res = res + soup_string[idx]
+
+    # printing result
+    print("The extracted logo string : " + res)
+    return str(res)
+
+
+# Capture Number of Results and other text elements
+def retrieve_elements(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    soup_string = str(soup)
+    # print(soup_string)
+
+    logo = find_elements_from_source(soup_string,
+                                     substring_to_find_elements["logo"]["str1"],
+                                     substring_to_find_elements["logo"]["str2"]
+                                     )
+    mcc = find_elements_from_source(soup_string,
+                                    substring_to_find_elements["mcc"]["str1"],
+                                    substring_to_find_elements["mcc"]["str2"]
+                                    )
+    merchant_name = find_elements_from_source(soup_string,
+                                              substring_to_find_elements["merchant_name"]["str1"],
+                                              substring_to_find_elements["merchant_name"]["str2"]
+                                              )
+
+    # creating a dictionary
+    retrieved_element_names = {"logo": logo, "mcc": mcc, "merchant_name": merchant_name}
+
+    return retrieved_element_names
+
+
+# Export DataFrame to CSV
+def export_to_csv(df, output_file_path):
+    df.to_csv(output_file_path, index=False)
+
+
+# Main function to control the flow
+def main():
+    driver = initialize_driver()
+    keywords_df = read_keywords_from_csv(inputFile)
+
+    for index, row in keywords_df.iterrows():
+        site = website + row['keyword']
+        driver.get(site)
+        start_time = time.time()
+        page_load_time = capture_page_load_time(start_time)
+        # wait for page to load
+        time.sleep(average_page_load_time)
+        elements_retrieved = retrieve_elements(driver)
+        take_screenshot(driver, row['keyword'], f"{index}.png")
+        # Copy text elements and other related metrics.
+        save_metrics(keywords_df, index, page_load_time, elements_retrieved)
+
+    export_to_csv(keywords_df, outputFile)
+    driver.quit()
+
+
+# Run the main function
+main()
